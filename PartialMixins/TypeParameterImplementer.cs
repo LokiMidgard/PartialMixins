5e47a6896adf1c58d35d7a0f74918aac01aaedf7
Patrick Kranz <patrick-kranz@live.de>
2016-04-06 17:22:01 +0200

    Generics werden nun Unterstütut

--- PartialMixins/TypeParameterImplementer.cs
+++ PartialMixins/TypeParameterImplementer.cs
@@ -21,15 +21,44 @@ namespace PartialMixins
             this.semanticModel = semanticModel;
         }
 
+        public override SyntaxNode VisitGenericName(GenericNameSyntax node)
+        {
+            if (!(node.Parent is QualifiedNameSyntax))
+            {
+                var info = semanticModel.GetSymbolInfo(node).Symbol as INamedTypeSymbol;
+                var newArguments = node.TypeArgumentList.Arguments.Select(x =>
+                {
+                    if (x is IdentifierNameSyntax)
+                        return (TypeSyntax)VisitIdentifierName(x as IdentifierNameSyntax);
+                    if (x is QualifiedNameSyntax)
+                        return (TypeSyntax)VisitQualifiedName(x as QualifiedNameSyntax);
+                    if (x is GenericNameSyntax)
+                        return (TypeSyntax)VisitGenericName(x as GenericNameSyntax);
+
+                    return x;
+                });
+                node = node.WithTypeArgumentList(SyntaxFactory.TypeArgumentList(SyntaxFactory.SeparatedList(newArguments)));
+
+                return SyntaxFactory.QualifiedName(SyntaxFactory.ParseName($"global::{PartialMixin.GetNsName(info.ContainingNamespace)}"), node);
+
+            }
+            return base.VisitGenericName(node);
+        }
+
+        public async Task<string> bl()
+        {
+            return null;
+        }
+
         public override SyntaxNode VisitQualifiedName(QualifiedNameSyntax node)
         {
             var rightText = node.Right.ToFullString();
             var leftText = node.Left.ToFullString();
-            if (!leftText.StartsWith("global"))
+            if (!leftText.StartsWith("global::"))
             {
                 var info = semanticModel.GetSymbolInfo(node).Symbol;
                 string fullQualifeidName;
-                if (info is IMethodSymbol)
+                if (info is IMethodSymbol && info.Name == ".ctor")
                     fullQualifeidName = $"global::{PartialMixin.GetNsName(info.ContainingNamespace)}.{(info as IMethodSymbol).ReceiverType.Name}";
                 else
                     fullQualifeidName = $"global::{PartialMixin.GetFullQualifiedName(info)}";
@@ -45,6 +74,8 @@ namespace PartialMixins
             var info = semanticModel.GetSymbolInfo(node);
             if (info.Symbol is ITypeParameterSymbol)
             {
+                if (node.Parent is TypeParameterConstraintClauseSyntax)
+                    return node;
                 var tSymbol = info.Symbol as ITypeParameterSymbol;
                 if (typeParameterMapping.ContainsKey(tSymbol))
                 {
@@ -57,8 +88,9 @@ namespace PartialMixins
                 if (info.Symbol != null
                     && !node.IsVar
                     && (info.Symbol.Kind == SymbolKind.ArrayType
+
                         || info.Symbol.Kind == SymbolKind.NamedType))
-                    return SyntaxFactory.ParseName($"global::{PartialMixin. GetFullQualifiedName(info.Symbol)}");
+                    return SyntaxFactory.ParseName($"global::{PartialMixin.GetFullQualifiedName(info.Symbol)}");
                 else if (info.Symbol != null
                     && !node.IsVar
                     && ((info.Symbol.Kind == SymbolKind.Method
